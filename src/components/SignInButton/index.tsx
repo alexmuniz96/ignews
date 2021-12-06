ddb1cfe60e03c6454fe8f60aef4d4d21b83bf90b
alexmuniz96 <alexmuniz096@gmail.com>
2021-12-06 16:38:09 -0300

    add authentication

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,26 +1,33 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true
-  return isUserLoggedIn ? (
+
+  const { data: session } = useSession()
+
+  return session ? (
     <button
       type='button'
       className={styles.signInButton}
     >
       <FaGithub color="#04d361" />
-      Alex Muniz
-      <FiX color="#737380" className={styles.closeIcon} />
+      {session.user.name}
+      <FiX
+        color="#737380"
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
     <button
       type='button'
       className={styles.signInButton}
+      onClick={() => signIn()}
     >
       <FaGithub color="#eba417" />
-      {console.log("está deslogado")}
       Sign in with Github
     </button>
   )
