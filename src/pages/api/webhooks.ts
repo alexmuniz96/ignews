d7e04adee2b3413b6cdd2667c6ce82454d7ccc72
alexmuniz96 <alexmuniz096@gmail.com>
2021-12-13 09:51:39 -0300

    fixed duplication of inscription in fauna

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -24,7 +24,6 @@ export const config = {
 
 const relevantEvents = new Set([
   'checkout.session.completed',
-  'customer.subscription.created',
   'customer.subscription.updated',
   'customer.subscription.deleted',
 ])
@@ -46,7 +45,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       if (relevantEvents.has(type)) {
         try {
           switch (type) {
-            case 'customer.subscription.created':
             case 'customer.subscription.updated':
             case 'customer.subscription.deleted':
 
@@ -55,7 +53,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
               await saveSubscription(
                 subscription.id,
                 subscription.customer.toString(),
-                type === 'customer.subscription.created',
+                false,
               );
 
               break;
